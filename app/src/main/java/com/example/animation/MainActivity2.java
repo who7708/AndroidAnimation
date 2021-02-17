31079e2ae72ccebd07c16949758d04b8423245a2
Chris <who7708@163.com>
2021-02-17 16:13:42 +0800

    android 动画

---
+++ app/src/main/java/com/example/animation/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.example.animation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.animation.R;
+
+public class MainActivity2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main2);
+    }
+
+}
